e51daefe9750e104e598212012ba5031c67e2e1f
Frank Jung <frankhjung@linux.com>
2021-11-17 10:37:50 +1100

    add ansible lint

--- apply.sh
+++ apply.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-# apply playbook
-ansible-playbook site.yml $*
+# This is a wrapper around ansible-playbook just so you don't need to type all
+# the parameters. There is also a project lint option.
+
+# set -o xtrace
+
+if [[ "$1" == "lint" ]]; then
+  # lint all playbooks excluding git warning
+  ansible-lint site.yaml
+else
+  # apply playbook to install and configure dokuwiki
+  # shellcheck disable=SC2048 # need white space in var
+  # shellcheck disable=SC2086 # allow word splitting
+  ansible-playbook -i inventory.yaml -c local -e target="$(uname -n)" site.yaml $*
+  rm -f site.retry scrapbook.retry
+fi
+
+# EOF
